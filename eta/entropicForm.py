d9e4cc2bb1d582b6ef4bad9d6bc14ac0ac59a6db
Rubens <rubens.sautter@gmail.com>
2023-11-26 22:25:06 -0300

    Reroll

--- eta/entropicForm.py
+++ eta/entropicForm.py
@@ -4,17 +4,15 @@ def shannon(p):
   p2 = p[p>1e-13]
   return -np.sum(p2*np.log(p2))/np.log(len(p))
 
-'''
+
 def tsallis(p,q=None,eps = None):
   qi = 1 if q is None else q
   epsi = 1e-4 if eps is None else eps
 
   if np.abs(qi-1)>epsi:
-    norm = (1 - (1/len(p))**qi)/(qi-1)
-    return ((1 - np.sum(np.power(p,qi)))/(qi-1))/norm
+    return (1 - np.sum(np.power(p,qi)))/(qi-1)
   else:
-    norm = (1 - (1/len(p))**(qi+epsi))/(qi+epsi-1)
-    return 	((1 - np.sum(np.power(p,qi+epsi)))/(qi+epsi-1))/norm
+    return 	(1 - np.sum(np.power(p,qi+epsi)))/(qi+epsi-1)
 '''
 def tsallis(p,q=None,eps = None):
   qi = 1 if q is None else q
@@ -26,7 +24,7 @@ def tsallis(p,q=None,eps = None):
   else:
     norm = (1 - (1/len(p))**(qi+epsi))
     return 	(1 - np.sum(np.power(p,qi+epsi)))/norm
-
+'''
 
 def plTsallis(p,qs=None,eps=None,returnQH=False):
   ts = []
@@ -34,7 +32,7 @@ def plTsallis(p,qs=None,eps=None,returnQH=False):
   epsi = 1e-4 if eps is None else eps
   for q in qsi:
     ts.append(tsallis(p,q,eps))
-  alpha, _, _ = np.polyfit(np.exp(qsi),np.log(ts),deg=2)
+  alpha,  _ = np.polyfit(np.log(qsi),np.log(ts),deg=1)
   if not(returnQH):
     return alpha
   else:
